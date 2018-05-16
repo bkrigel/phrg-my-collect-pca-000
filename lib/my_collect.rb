0ccf330003b90491aafc0d34f212e8e46408ae8a
bkrigel <benjamin.krigel@gmail.com>
2018-05-16 08:38:58 -0400

    There was a small name change '[]\';

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(new_collection)
+  i = 0
+  collection = []
+    while i < new_collection.length
+      collection << yield(new_collection[i])
+      i += 1
+    end
+      collection
+end
 
